7cedfef52a8393b2ad958aca20fbd86181c65022
toy <toy@ownway.info>
2017-04-01 17:56:49 +0900

    Apache の設定に必要なものを足した。

--- provision_setup_apache2.sh
+++ provision_setup_apache2.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-apt-get apache2 -y
+apt-get install apache2 -y
 apt-get install php7.0 -y
+apt-get install php7.0-mbstring
+apt-get install php7.0-sqlite3
+apt-get install php-pear -y
 apt-get install libapache2-mod-php7.0 -y
