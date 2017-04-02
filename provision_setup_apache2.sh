07f9e1af1fa8891f45dfa2fcb3a7f46d471a5637
toy <toy@ownway.info>
2017-04-02 17:12:07 +0900

    Apache2 の rewrite を有効にした。

--- provision_setup_apache2.sh
+++ provision_setup_apache2.sh
@@ -3,6 +3,11 @@
 echo "[`date`] Start provision setup_apache2"
 
 apt-get install -y apache2
+
+# Mod
+a2enmod rewrite
+
+# Site
 cd /etc/apache2/sites-available
 cp 000-default.conf 001-www.conf
 sed -i -e "s/webmaster@localhost/toy@ownway.info/" 001-www.conf
