7b6758305894e9adef99c5703f06cdb1b6838bf2
toy <toy@ownway.info>
2017-05-17 23:47:06 +0900

    apt-get -y update を最初のセットアップに追加した。

--- provision/setup_precondition.sh
+++ provision/setup_precondition.sh
@@ -2,6 +2,8 @@
 
 echo "[`date`] Start provision setup_precondition"
 
+apt-get -y update
+
 sudo curl -o /usr/local/bin/jq -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 && sudo chmod +x /usr/local/bin/jq
 
 echo "[`date`] End   provision setup_precondition"
