c21747a77abaa5f95900584d5df73288db36499e
toydev <toy@ownway.info>
2019-08-07 15:02:25 +0900

    Ubuntu のバージョンを 19.04 に更新した。PHP のバージョンも上げた。

--- provision/setup_mysql.sh
+++ provision/setup_mysql.sh
@@ -5,7 +5,7 @@ echo "[`date`] Start provision `basename $0`"
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password 1234qwer'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 1234qwer'
 apt install -y mysql-server
-apt install -y php7.0-mysql
+apt install -y php-mysql
 
 cd /etc/mysql/mysql.conf.d
 sed -i -e "s/^bind-address/#bind-address/" mysqld.cnf
--- provision/setup_php.sh
+++ provision/setup_php.sh
@@ -2,16 +2,16 @@
 
 echo "[`date`] Start provision `basename $0`"
 
-apt install -y php7.0
-apt install -y php7.0-mbstring
-apt install -y php7.0-sqlite3
+apt install -y php
+apt install -y php-mbstring
+apt install -y php-sqlite3
 apt install -y php-pear
-apt install -y libapache2-mod-php7.0
+apt install -y libapache2-mod-php
 
-apt install -y php7.0-dev
+apt install -y php-dev
+pecl channel-update pecl.php.net
 pecl install xdebug
-cd /etc/php/7.0/apache2/conf.d
-cp /vagrant/provision/support/xdebug.ini /etc/php/7.0/mods-available
-ln -s ../../mods-available/xdebug.ini 20-xdebug.ini
+cp /vagrant/provision/support/xdebug.ini /etc/php/7.2/mods-available
+ln -s /etc/php/7.2/mods-available/xdebug.ini /etc/php/7.2/apache2/conf.d/20-xdebug.ini
 
 echo "[`date`] End   provision `basename $0`"
